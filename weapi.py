02a8465a6bb42b35e82ea5eb39026002d4f06641
catchsob <42635950+catchsob@users.noreply.github.com>
2024-01-07 22:40:28 +0800

    support multi-site

--- weapi.py
+++ weapi.py
@@ -1,18 +1,27 @@
 # pip install Flask
 
+# version 0.1.0: support multi-site
+
 from flask import Flask, request
 from wea import WeaG
 
-app = Flask(__name__)
+__version__ = '0.1.0'
+
+app = Flask('weapi-'+__version__)
 
 w = WeaG()
 
 @app.route('/obs', methods=['POST'])
 def obs():
     if request.args and 'site' in request.args:
-        site = request.args['site']
-        r = w.grab(site)
-        j = {'success': True, site: r} if r else {'success': False, 'reason': 'no such site'}
+        rs = {}
+        j = {'success': False}
+        for site in request.args.getlist('site'):
+            r = w.grab(site)
+            rs[site] = r if r else None
+            if r:
+                j['success'] = True
+        j.update(rs)
     else:
         j = {'success': False, 'reason': 'query got no site'}
     print(j)
