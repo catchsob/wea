68076810be65191254c80d8361afe336fc1ab87c
catchsob <42635950+catchsob@users.noreply.github.com>
2024-05-05 08:47:35 +0800

    Add files via upload

--- wea.py
+++ wea.py
@@ -6,40 +6,49 @@ from bs4 import BeautifulSoup
 class WeaG:
     '''Weather Grabber, to grab observed weather information from official CWA website'''
 
-    def __init__(self):
+    URLS = {'site_obs': 'https://www.cwa.gov.tw/V8/C/W/Observe/MOD/24hr/TBD.html',
+            'site_map': 'https://www.cwa.gov.tw/Data/js/Observe/OSM/C/STMap.json'}
+
+    def __init__(self, verbose=False):
         '''to initialize the weather sites information'''
 
-        self._url = 'https://www.cwa.gov.tw/V8/C/W/Observe/MOD/24hr/TBD.html'
-        self._load()
+        self.verbose = verbose
+        self.sites = self._load_sitemap()
     
     def grab(self, site):
-        '''to grab observed rainfall, temperature, and humidity information from official CWA website
-           params - location: name of site, should be same as CWA official website
-           return - observation time, rainfall, temperature, and humidity in dict,
+        '''to grab observed temperature, humidity, and rainfall information from official CWA website
+           params - location: name of site, should be the same as CWA official website
+           return - observation time, temperature, humidity, and rainfall in dict,
                     ex: {'O': '11/02 11:20', 'T': 27.5, 'H': 0.73, 'R': 0.0}
         '''
 
-        rtn = {}
+        obs = {}
         if site in self.sites:
-            url = self._url.replace('TBD', self.sites[site])
+            url = __class__.URLS['site_obs'].replace('TBD', self.sites[site])
             r = requests.get(url)
             if r.status_code == 200:
                 soup = BeautifulSoup(r.text, 'html.parser')
                 try:
-                    rtn['O'] = soup.find(headers="time").text
-                    rtn['T'] = float(soup.find(class_="tem-C").text)
-                    rtn['H'] = float(soup.find(headers="hum").text)/100
-                    rtn['R'] = float(soup.find(headers="rain").text)
-                except:
-                    pass
+                    obs['O'] = soup.find(headers="time").text
+                    obs['T'] = float(soup.find(class_="tem-C").text)
+                    obs['H'] = float(soup.find(headers="hum").text)/100
+                    obs['R'] = float(soup.find(headers="rain").text)
+                except Exception as e:
+                    if self.verbose:
+                        print(f'grab {site} got {e}')
             r.close()
-        return rtn
+        
+        if self.verbose:
+            print(f'grab {site} got {obs}')
+        return obs
     
-    def _load(self):
-        url = 'https://www.cwa.gov.tw/Data/js/Observe/OSM/C/STMap.json'
-        r = requests.get(url)
-        self.sites = {i['STname']: i['ID'] for i in r.json()} if r.status_code == 200 else {}
+    def _load_sitemap(self):
+        r = requests.get(__class__.URLS['site_map'])
+        sites = {i['STname']: i['ID'] for i in r.json()} if r.status_code == 200 else {}
         r.close()
+        if self.verbose:
+            print(f"{len(sites)} sites are loaded from {__class__.URLS['site_map'].split('/')[-1]}")
+        return sites
 
 
 if __name__ == '__main__':
@@ -48,13 +57,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('sites', nargs='*', default=['臺北'],
                         help='weather of site to be grabbed, default 臺北')
+    parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
     
-    w = WeaG()
+    w = WeaG(args.verbose)
     for site in args.sites:
-        r = w.grab(site)
-        if r:
-            print(r)
+        if r := w.grab(site):
             print(f'{site} 觀測時間: {r["O"]} 溫度: {r["T"]:.1f}°C, 濕度: {r["H"]:.0%}, 雨量: {r["R"]:.1f}mm')
         else:
             print(f'{site} 查無此站！')
